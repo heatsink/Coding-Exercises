c589a816727acc755ab5e8cb2dc433bd1cdff197
heatsink <seanswally@gmail.com>
2016-12-28 16:39:27 -1000

    Fix promotion test

---
+++ Ch2/promotion.c
@@ -0,0 +1,33 @@
+/**
+ * This program tests promotions in c.
+ * @author Sean Wallace
+ * @version 1
+ */
+#include <stdio.h>
+
+/**
+ * This is the main method.
+ */
+int main() {
+    if (-1) {
+        printf("K&R doesn't Lie.\n");
+    }
+    else {
+        printf("K&R Lied to me.\n");
+    }
+    if (9001) {
+        printf("K&R doesn't Lie.\n");
+    }
+    else {
+        printf("K&R Lied to me.\n");
+    }
+    if (0) {
+        printf("K&R Lied to me.\n");
+    }
+    else {
+        printf("K&R doesn't Lie.\n");
+    }
+
+
+    return 0;
+}
