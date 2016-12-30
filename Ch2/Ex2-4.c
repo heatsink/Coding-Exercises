b124c20074faa58381cded78f1683407b7fad240
heatsink <seanswally@gmail.com>
2016-12-29 14:58:50 -1000

    Fix comment from not working to working status

--- Ch2/Ex2-4.c
+++ Ch2/Ex2-4.c
@@ -1,5 +1,4 @@
 /**
- * Currently not working. Pushed because these are my solo exercises for backup.
  * This program fulfils Exercise 2-4.
  * Write an alternative version of squeeze(s1,s2) that deletes each character 
  * in s1 that matches any character in the string s2.
