40cccf6fc9ee021ae4665d17aa46e36a8d894cc7
Sean Wallace <seanswally@gmail.com>
2017-01-28 00:25:16 -0800

    Add hello world in ruby and print an array in C++

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
+   int arr[10];
+   int i;
+   for (i = 0; i < 10; i++) {
+      arr[i] = i;
+   }
+   for (i = 0; i < 10; i++) {
+      cout << "arr[";
+      cout << + i;
+      cout << "]: ";
+      cout << arr[i];
+      cout << "\n";
+   }
    cout << "Hello world! I'm written in C++!\n";
    return 0;
 }
